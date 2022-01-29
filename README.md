aa9108fab5f1948fe4dd9da6fde586d27a4ae745
falovic <mail@falovic.com>
2022-01-29 05:28:16 +0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Guess The Number
+# Guess the Number
 
 > - Sayı tahmin etme oyunu.
 > - Toplam 5 hakkınız var.
---
+++ script.js
@@ -0,0 +1,35 @@
+// Guess the Number
+
+// 1-10 arası rastgele üretilen bir sayıyı aşağı / yukarı ifadeleri ile buldurmaya çalışın.
+// Puanlama yapın.
+
+var hak = 5;
+var tahmin;
+var sayac = 0;
+var sayi = Math.floor((Math.random() * 10) + 1);
+
+console.log(sayi);
+
+while (hak > 0) {
+    hak--;
+    sayac++;
+    tahmin = Number(prompt('1-10 arasında bir sayı giriniz.'));
+
+    if (sayi == tahmin) {
+        console.log(`Tebrikler ${sayac} defada bildiniz.`);
+        console.log(`Puanınız: ${100 - (20)*(sayac-1)}`);
+        alert(`Tebrikler ${sayac} defada bildiniz. Puanınız: ${100 - (20)*(sayac-1)}`);
+        break;
+    } else if (sayi > tahmin) {
+        console.log('Yukarı');
+        alert(`Yukarı ( Kalan hakkınız: ${hak} )`);
+
+    } else {
+        console.log('Aşağı');
+        alert(`Aşağı ( Kalan hakkınız: ${hak} )`);
+    }
+    if (hak == 0) {
+        console.log('Bilemediniz.')
+        alert(`Ahahah bilemediniz. Puanınız: 0`);
+    }
+}
